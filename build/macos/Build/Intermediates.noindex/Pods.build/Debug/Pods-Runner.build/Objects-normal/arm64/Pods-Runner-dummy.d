c46c827462f1c32f04eb12483152d19b0dee98ac
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2023-04-03 14:54:44 +0200

    first commit

---
+++ build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/sambachamproux/development/my_app/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Users/sambachamproux/development/my_app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
